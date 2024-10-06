f314f58f12c3f11a0c6750dc1fc8d9c7207b616f
jossamador <jos-amador@hotmail.com>
2024-10-06 11:36:58 -0600

    practica10

---
+++ Practica10/index.js
@@ -0,0 +1,23 @@
+const os = require('os');
+console.log('Sistema operativo:', os.platform());
+
+const platform = os.platform();
+console.log(platform);
+
+const nameOs = os.type();
+console.log(nameOs);
+
+const uptime = os.uptime();
+console.log(uptime);
+
+const totalMemory = os.totalmem();  
+console.log(totalMemory);
+
+const cpuArch = os.arch();
+console.log(cpuArch);
+
+const cpuCoreLogic = os.cpus();
+console.log(cpuCoreLogic);
+
+const networkInterfaces = os.networkInterfaces();
+console.log(networkInterfaces);
